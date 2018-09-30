c5ad2ddb38666da056942092a5742016a01fe320
Jérôme Heuzé <jeromeheuze@gmail.com>
2018-09-30 00:02:28 -0700

    updated js and logic

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 /*
  * Create a list that holds all of your cards
  */
-const cards = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+const cards = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb","fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
 
-const ul = document.getElementById("masterdeck");
-
+let ul = document.getElementById("masterdeck");
+let panelWon = document.getElementsByClassName("game-won");
+let cardOpen = 0;
+let cardTracker = [];
+let wait = "";
+let totalCards = cards.length;
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -15,13 +19,12 @@ const ul = document.getElementById("masterdeck");
 function displayDeck(array) {
     let cardOuput = "";
     for (let card of array) {
-        cardOuput += '<li class="card open show">\n' +
+        cardOuput += '<li class="card closed">\n' +
             '                <i class="fa '+card+'"></i>\n' +
-            '            </li>';
+            '            </li>\n';
     }
-    console.log(cardOuput);
+    //console.log(cardOuput);
     ul.innerHTML = cardOuput;
-
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -41,16 +44,171 @@ function shuffle(array) {
 
 function initDeck() {
     //create array of cards
-    let cardSet = [...shuffle(cards),...shuffle(cards)];
-    console.log(cardSet);
+    let cardSet = [...shuffle(cards)];
+    //console.log(cardSet);
 
     //empty deck
     ul.innerHTML = '';
+
+    //remove win screen
+    panelWon[0].classList.remove("show");
 
     displayDeck(cardSet);
     return false;
 }
 
+//restart game
+function restartGame() {
+    //reset cards
+    initDeck();
+    //zero moves
+    let counter = document.getElementsByClassName("moves");
+    counter[0].innerHTML = "0";
+    //remove stars for moves
+
+}
+
+//wining game
+function gameWon() {
+    panelWon[0].classList.add("show");
+}
+
+//stars logic
+function starsLogic(moves) {
+    let panelStars = document.getElementsByClassName("stars");
+    console.log(panelStars);
+    let starsValue = "0";
+    if (moves > cards.length / 2) { // 1 star
+        //panelStars[0].children[0].classList.add("fa");
+        panelStars[0].children[0].classList.remove("fa-star-o");
+        panelStars[0].children[0].classList.add("fa-star");
+
+       // panelStars[0].children[1].classList.add("fa");
+        panelStars[0].children[1].classList.add("fa-star-o");
+
+        //panelStars[0].children[2].classList.add("fa");
+        panelStars[0].children[2].classList.add("fa-star-o");
+        starsValue = "1";
+    } else if (moves < cards.length / 2) { // 2 stars
+        //panelStars[0].children[0].classList.add("fa");
+        panelStars[0].children[0].classList.remove("fa-star-o");
+        panelStars[0].children[0].classList.add("fa-star");
+
+        //panelStars[0].children[1].classList.add("fa");
+        panelStars[0].children[1].classList.remove("fa-star-o");
+        panelStars[0].children[1].classList.add("fa-star");
+
+        //panelStars[0].children[2].classList.add("fa");
+        panelStars[0].children[2].classList.add("fa-star-o");
+        starsValue = "2";
+    } else if (moves === cards.length / 2) { // 3 stars
+        //panelStars[0].children[0].classList.add("fa");
+        panelStars[0].children[0].classList.remove("fa-star-o");
+        panelStars[0].children[0].classList.add("fa-star");
+
+        //panelStars[0].children[1].classList.add("fa");
+        panelStars[0].children[1].classList.remove("fa-star-o");
+        panelStars[0].children[1].classList.add("fa-star");
+
+        //panelStars[0].children[2].classList.add("fa");
+        panelStars[0].children[2].classList.remove("fa-star-o");
+        panelStars[0].children[2].classList.add("fa-star");
+        starsValue = "3";
+    }
+
+    let panelWonStars = document.getElementsByClassName("starsText");
+    panelWonStars[0].innerHTML = starsValue;
+}
+
+//restart game
+function counterMove() {
+    let counter = document.getElementsByClassName("moves");
+    //get current count
+    let currentCounter = counter[0].innerHTML;
+    currentCounter = parseInt(currentCounter) + 1;
+    counter[0].innerText = currentCounter;
+    //populate winning screen
+    let panelWonMoves = document.getElementsByClassName("movesText");
+    panelWonMoves[0].innerHTML = currentCounter;
+    starsLogic(currentCounter);
+}
+
+//check matching cards
+function isMatching(crd1, crd2) {
+    if (crd1 === crd2) {
+        totalCards = totalCards - 2;
+        if (totalCards === 0) { gameWon(); }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function foldCards() {
+    wait = setTimeout(function(){
+        cardTracker[0].classList.remove("open");
+        cardTracker[0].classList.remove("show");
+        cardTracker[0].classList.add("closed");
+        cardTracker[1].classList.remove("open");
+        cardTracker[1].classList.remove("show");
+        cardTracker[1].classList.add("closed");
+        cardTracker = [];
+        cardOpen = 0;
+    }, 1000);
+}
+
+function matchCards() {
+    wait = setTimeout(function(){
+        cardTracker[0].classList.add("match");
+        cardTracker[0].classList.remove("closed");
+
+        cardTracker[1].classList.add("match");
+        cardTracker[1].classList.remove("closed");
+        cardTracker = [];
+        cardOpen = 0;
+    }, 1000);
+}
+
+//logic card event
+function showCard(e) {
+    if (cardOpen === 0) {
+        e.target.classList.remove("closed");
+        e.target.classList.add("open");
+        e.target.classList.add("show");
+        cardTracker.push(e.target);
+    } else if (cardOpen === 1) {
+        e.target.classList.remove("closed");
+        e.target.classList.add("open");
+        e.target.classList.add("show");
+        cardTracker.push(e.target);
+        //two cards are up
+        counterMove();
+        if (isMatching(cardTracker[0].lastElementChild.className, cardTracker[1].lastElementChild.className)) {
+            //matching keep
+            matchCards();
+        } else {
+            //does not match fold
+            foldCards();
+        }
+    }
+    cardOpen++;
+}
+
+//click events manager
+document.addEventListener('click', function (event) {
+
+    //list of click events on the page
+    if (event.target.matches('.fa-repeat')) { restartGame() }
+    if (event.target.matches('.card.closed')) { showCard(event); }
+    if (event.target.matches('.btnPlayAgain')) { initDeck(); }
+
+    // Don't follow the link
+    event.preventDefault();
+
+    // Log the clicked element in the console
+    //console.log(event.target);
+
+}, false);
 initDeck();
 
 /*
